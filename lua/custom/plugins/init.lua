8c6f50ccc121b025c213ef008d6dc85fa540ea24
ugedal1 <ugedal1@hotmail.com>
2025-01-20 11:59:19 +0100

    feat: added oil

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,25 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
+
+    config = function()
+      require('oil').setup {
+
+        columns = { 'icon' },
+        keymaps = {
+          ['<M-h>'] = 'actions.select_split',
+        },
+        view_options = { show_hidden = true },
+      }
+      --open parent directory in current window
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'open parent directory' })
+
+      -- open parent directory in floating window
+      vim.keymap.set('n', '<space>-', require('oil').toggle_float)
+    end,
+  },
+}
